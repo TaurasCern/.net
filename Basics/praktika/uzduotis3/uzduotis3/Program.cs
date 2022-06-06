cb1af233ecd8b11b141e85463aca043d32c3ced3
Taurenxoo <t.cerniauskas@yahoo.com>
2022-06-07 01:38:41 +0300

    ikeliama trecia uzduotis

---
+++ Basics/praktika/uzduotis3/uzduotis3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace uzduotis3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> face = new List<string>();
+            face.Add("    ***************    ");
+            face.Add("  **               **  ");
+            face.Add(" **                 ** ");
+            face.Add(" **     ()     ()   ** ");
+            face.Add(" **         |       ** ");
+            face.Add(" **         |       ** ");
+            face.Add("  **    --------   **  ");
+            face.Add("    ***************    ");
+
+            foreach(string line in face)
+            {
+                Console.WriteLine(line);
+            }
+
+            foreach(string line in face)
+            {
+                Console.WriteLine(line.Replace('*', '"'));
+            }
+        }
+    }
+}
