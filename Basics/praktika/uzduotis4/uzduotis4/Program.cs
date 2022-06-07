79bb4b71b5cb4eb88ccfe4bdb51e9de6aa266b7f
Taurenxoo <t.cerniauskas@yahoo.com>
2022-06-07 19:22:13 +0300

    string pakeistai char, ir prideti indikatoriai kol veiksmas yra vykdomas

--- Basics/praktika/uzduotis4/uzduotis4/Program.cs
+++ Basics/praktika/uzduotis4/uzduotis4/Program.cs
@@ -4,45 +4,48 @@
     {
         static void Main(string[] args)
         {
-            string num1;
-            string num2;
+            char num1;
+            char num2;
 
             Console.WriteLine("Sudetis:");
             Console.WriteLine("Iveskite pirma skaiciu:");
-            num1 = Console.ReadLine();
-            Console.WriteLine("Iveskite antra skaiciu:");
-            num2 = Console.ReadLine();
-            Console.WriteLine("suma = {0}", Convert.ToInt32(num1[0]) - 48 + Convert.ToInt32(num2[0]) - 48);
+            num1 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nIveskite antra skaiciu:");
+            num2 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nsuma = {0}", Convert.ToInt32(num1) - 48 + Convert.ToInt32(num2) - 48);
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
 
+            Console.WriteLine("Atimtis:");
             Console.WriteLine("Iveskite trecia skaiciu:");
-            num1 = Console.ReadLine();
-            Console.WriteLine("Iveskite ketvirta skaiciu:");
-            num2 = Console.ReadLine();
-            Console.WriteLine("likutis = {0}", (Convert.ToInt32(num1[0]) - 48) - (Convert.ToInt32(num2[0]) - 48));
+            num1 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nIveskite ketvirta skaiciu:");
+            num2 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nlikutis = {0}", (Convert.ToInt32(num1) - 48) - (Convert.ToInt32(num2) - 48));
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
 
+            Console.WriteLine("Daugyba:");
             Console.WriteLine("Iveskite penkta skaiciu:");
-            num1 = Console.ReadLine();
-            Console.WriteLine("Iveskite sesta skaiciu:");
-            num2 = Console.ReadLine();
-            Console.WriteLine("sandauga = {0}", (Convert.ToInt32(num1[0]) - 48) * (Convert.ToInt32(num2[0]) - 48));
+            num1 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nIveskite sesta skaiciu:");
+            num2 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nsandauga = {0}", (Convert.ToInt32(num1) - 48) * (Convert.ToInt32(num2) - 48));
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
 
+            Console.WriteLine("Dalyba:");
             Console.WriteLine("Iveskite septinta skaiciu:");
-            num1 = Console.ReadLine();
-            Console.WriteLine("Iveskite astunta skaiciu:");
-            num2 = Console.ReadLine();
-            Console.WriteLine("dalmuo = {0}", (Convert.ToInt32(num1[0]) - 48) / (Convert.ToInt32(num2[0]) - 48));
+            num1 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\nIveskite astunta skaiciu:");
+            num2 = Console.ReadKey().KeyChar;
+            Console.WriteLine("\ndalmuo = {0}", (Convert.ToInt32(num1) - 48) / (Convert.ToInt32(num2) - 48));
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
