cbdcfbc6b17dd3e948e6ff426b05bcf3748388c4
Taurenxoo <t.cerniauskas@yahoo.com>
2022-06-07 18:56:20 +0300

    pakeista kad veiktu be if else

--- Basics/praktika/uzduotis1/uzduotis1/Program.cs
+++ Basics/praktika/uzduotis1/uzduotis1/Program.cs
@@ -5,40 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Iveskite savo varda:");
-            var letter = Console.ReadLine()[0];
+            char letter = Console.ReadLine()[0];
             Console.WriteLine("Pirmoji vardo raide: {0}, raides ASCII kodas: {1}", letter, Convert.ToInt32(letter));
             Console.WriteLine();
 
             Console.Write("Iveskite skaiciu:");
-            var sum = Convert.ToInt32(letter) + int.Parse(Console.ReadLine());
-            Console.WriteLine("ASCII kodas sudetasa su ivestu skaiciumi: {0:C}", sum);
+
+            Console.WriteLine("ASCII kodas sudetasa su ivestu skaiciumi: {0:C}", Convert.ToInt32(letter) + int.Parse(Console.ReadLine()));
 
             Console.WriteLine();
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
             Console.Clear();
 
-            Console.WriteLine("Meniu:");
-            Console.WriteLine("(1) Pirkti");
-            Console.WriteLine("(2) Parduoti");
-            Console.WriteLine("(3) Likuciai");
-            string choice = Console.ReadLine();
-            Console.Clear();
-            if(choice == "1")
-            {
-                Console.WriteLine("Pasirinkote pirkti.");
-            }
-            else if(choice == "2")
-            {
-                Console.WriteLine("Pasirinkote parduoti.");
-            }
-            else if(choice == "3")
-            {
-                Console.WriteLine("Pasirinkote likuciai.");
-            }else
-            {
-                Console.WriteLine("Klaida.");
-            }
+            Console.WriteLine("Meniu:\n(1) Pirkti\n(2) Parduoti\n(3) Likuciai\n");
+            Console.WriteLine("\nPasirinktas {0}", Convert.ToInt32(Console.ReadKey().KeyChar) - 48);
             Console.WriteLine("Press any key to continue...");
             Console.WriteLine();
             Console.ReadKey();
