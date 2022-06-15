12a8f16659da7d870500602f0b778a93980a6c3c
Taurenxoo <t.cerniauskas@yahoo.com>
2022-06-15 13:39:50 +0300

    savarankiska 2-2, be grafikos

---
+++ Basics/praktika/savarankiskas2-2/savarankiskas2-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace savarankiskas2_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite atstuma(km) tarp tasku a ir b:");
+            double distance = double.Parse(Console.ReadLine());
+            Console.WriteLine("Iveskite pirmos transporto priemones greiti:");
+            double vehicleSpeed1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Iveskite antros transporto priemones greiti:");
+            double vehicleSpeed2 = double.Parse(Console.ReadLine());
+
+            double timeToMeet = distance / (vehicleSpeed1 + vehicleSpeed2); // laikas kada transporto priemones susitiks (valandomis)
+            double meetingPoint = vehicleSpeed1 * timeToMeet; // kokiame taske transporto priemones susitiks nuo A tasko (kilometrais)
+            double vehicleEndPointTime1 = distance / vehicleSpeed1;
+            double vehicleEndPointTime2 = distance / vehicleSpeed2;
+
+            Console.WriteLine("Susitikimo taskas nuo A tasko: {0}", meetingPoint);
+            Console.WriteLine("Laikas po kurio susitiks transporto priemones: {0}s", timeToMeet * 3600);
+            Console.WriteLine("Laikas po kurio transporto priemones pasieks kelio gala: pirma pasieks per {0}min, antra pasieks per {1}min", vehicleEndPointTime1 * 60, vehicleEndPointTime2 * 60);
+            Console.WriteLine("Abi transporto priemones isskyre {0} gramu CO2 per nuvaziuota kelia.", distance * 95 * 2);
+        }
+    }
+}
