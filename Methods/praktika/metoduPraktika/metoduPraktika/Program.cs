18f52b98dd017e464349ecae3a6d42ccfd922aaa
Taurenxoo <t.cerniauskas@yahoo.com>
2022-06-27 22:01:06 +0300

    praktika

---
+++ Methods/praktika/metoduPraktika/metoduPraktika/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace metoduPraktika
+{
+    internal class Program
+    {
+        /*
+        public static void Main(string[] args)
+        {
+            PrintHello();
+            PrintToScreen();
+        }
+
+        public static void PrintHello()
+        {
+            Console.WriteLine("Sveiki visi");
+        }
+
+        public static void PrintToScreen()
+        {
+            Console.WriteLine("Linkiu jums geros dienos");
+        }
+        */
+        /*
+        public static void Main(string[] args)
+        {
+            PrintToScreen();
+        }
+
+        public static string EnterName()
+        {
+            string name;
+            Console.WriteLine("Iveskite varda");
+            name = Console.ReadLine();
+            Console.WriteLine("Labas {0}", name);
+            return name;
+        }
+
+        public static void PrintToScreen()
+        {
+            Console.WriteLine("Linkiu jums {0} geros dienos", EnterName());
+        }
+        */
+        /*
+        public static void Main(string[] args)
+        {
+            bool in1 = int.TryParse(Console.ReadLine(), out int num1);
+            if(!in1) { Console.WriteLine("blogai ivestas skaicius"); }
+            bool in2 = int.TryParse(Console.ReadLine(), out int num2);
+            if (!in2) { Console.WriteLine("blogai ivestas skaicius"); }
+            Sum(num1, num2);
+        }
+
+        public static void Sum(int num1, int num2)
+        {
+            Console.WriteLine("Rezultatas: {0}", num1 + num2);
+        }
+        */
+        /*
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            WhiteSpaceCount(text);
+        }
+
+        public static void WhiteSpaceCount(string text)
+        {
+            int count = text.Replace(" ", "").Length;
+            Console.WriteLine("Tarpu kiekis: {0}", text.Length - count);
+        } 
+        */
+        
+        public static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            TextLength(text);
+        }
+
+        public static void TextLength (string text)
+        {
+            string trimmed = text.Trim();
+            Console.WriteLine("Teksto ilgis yra: {0}", trimmed.Length);
+        }
+        
+    }
+}
