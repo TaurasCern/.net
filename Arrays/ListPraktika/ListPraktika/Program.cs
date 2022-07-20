cea69d1ce61df6acf2168af8599d882ed7be8631
Taurenxoo <t.cerniauskas@yahoo.com>
2022-07-20 22:09:11 +0300

    atlikta praktika

--- Arrays/ListPraktika/ListPraktika/Program.cs
+++ Arrays/ListPraktika/ListPraktika/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace ListPraktika
+﻿using System.Text;
+
+namespace ListPraktika
 {
     public class Program
     {
         static void Main(string[] args)
         {
         }
+        // 07-19
         public static int FindHighestNumber(List<int> numbers) => numbers.Max();
         public static List<int> DidesnisUzDidziausia(List<int> numbers)
         {
@@ -12,7 +15,7 @@
             return numbers;
         }
         public static List<int> DidesnisUzDidziausia1(List<int> numbers)
-        { 
+        {
             numbers.Sort();
             numbers.Add(numbers[numbers.Count - 1] + 1);
             return numbers;
@@ -25,14 +28,12 @@
         public static int FindHighestNumber1(List<int> numbers)
         {
             int max = numbers[0];
-            foreach(int number in numbers)
+            foreach (int number in numbers)
             {
-                if(max < number) { max = number; }
+                if (max < number) { max = number; }
             }
             return max;
         }
-
-
         public static int FindLowestNumber(List<int> numbers)
         {
             int min = numbers[0];
@@ -51,6 +52,102 @@
             }
             return numbers;
         }
-
+        //---------------------------------------------------------------------------
+        // 07-20
+        /// <summary>
+        /// 1
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static double Avg(List<int> numbers)
+        {
+            double avg = 0;
+            foreach (int number in numbers)
+            {
+                avg += number;
+            }
+            return avg / numbers.Count;
+        }
+        /// <summary>
+        /// 2
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public static List<string> IsPositive(List<int> numbers)
+        {
+            List<string> result = new List<string>();
+            foreach (int number in numbers)
+            {
+                if (number >= 0) { result.Add("Teigiamas"); }
+                else if (number < 0) { result.Add("Neigiamas"); }
+            }
+            return result;
+        }
+        /// <summary>
+        /// 3
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="gpm"></param>
+        /// <returns></returns>
+        public static double CalculateGPM(List<double> numbers, int gpm)
+        {
+            double result = 0;
+            foreach (double number in numbers)
+            {
+                result += number;
+            }
+            return result * (gpm / 100d);
+        }
+        /// <summary>
+        /// 4
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static List<int> IstrauktiSkaicius(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(char ch in text)
+            {
+                if (Char.IsDigit(ch)) { sb.Append(ch); }
+            }
+            return Sort(sb.ToString());
+        }
+        public static List<int> Sort(string numbersInText)
+        {
+            List<int> numbers = new List<int>();
+            foreach(char ch in numbersInText)
+            {
+                numbers.Add(ch - 48);
+            }
+            numbers.Sort();
+            return numbers;
+        }
+        /// <summary>
+        /// 5
+        /// </summary>
+        public static List<string> IsmetytiZodziai(string sentence)
+        {
+            List<string> words = new List<string>();
+            string[] wordsArr = sentence.Split(" ");
+            foreach(string word in wordsArr)
+            {
+                if(word.Length >= 5) { words.Add(word); }              
+            }
+            words.Sort();
+            return words;
+        }
+        public static List<string> CombineLists(List<string> a, List<string> b)
+        {
+            List<string> combined = new List<string>();
+            foreach(string st in a)
+            {
+                combined.Add(st);
+            }
+            foreach (string st in b)
+            {
+                combined.Add(st);
+            }
+            return combined;
+        }
     }
 }
--- Arrays/ListPraktika/ListTests/UnitTest1.cs
+++ Arrays/ListPraktika/ListTests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace ListTests
 {
     [TestClass]
@@ -49,6 +51,70 @@ namespace ListTests
             var numbers = new List<int>() { 5, 1, 6, 8, 7, 1 };
             var actual = ListPraktika.Program.RemoveLowestNumber(numbers);
             var expected = new List<int>() { 5, 6, 8, 7 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Avg_Test()
+        {
+            var numbers = new List<int>() { 5, 10, 5, 0 };
+            var actual = ListPraktika.Program.Avg(numbers);
+            var expected = 5d;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void IsPositive_Test()
+        {
+            var numbers = new List<int>() { 5, 10, -5, 0 };
+            var actual = ListPraktika.Program.IsPositive(numbers);
+            var expected = new List<string> { "Teigiamas", "Teigiamas", "Neigiamas", "Teigiamas" };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void CalculateGPM_Test()
+        {
+            var numbers = new List<double>() { 100, 200, 300, 400 };
+            var actual = ListPraktika.Program.CalculateGPM(numbers,15);
+            var expected = 150d;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void IstrauktiSkaicius_Test()
+        {
+            var fake = "sadc5sdcsa5cds1cdscds2dcsd";
+            var actual = ListPraktika.Program.IstrauktiSkaicius(fake);
+            var expected = new List<int> { 1, 2, 5, 5};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Sort_Test()
+        {
+            var fake = "5512";
+            var actual = ListPraktika.Program.Sort(fake);
+            var expected = new List<int> { 1, 2, 5, 5 };
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void IstrauktiSkaicius_Test1()
+        {
+            var fake = "sadc5sdcsa5cds1cdscds2dcsd";
+            var actual = String.Join("", ListPraktika.Program.IstrauktiSkaicius(fake));
+            var expected = "1255";
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void IsmetytiZodziai_Test()
+        {
+            var fake = "aa bbbbb ee ddd ff zzzzz aaaaaa";
+            var actual = ListPraktika.Program.IsmetytiZodziai(fake);
+            var expected = new List<string> {"aaaaaa", "bbbbb", "zzzzz"};
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        public void CombineLists_Test()
+        {
+            var fake1 = new List<string>() { "aaa", "aaa", "aaa"};
+            var fake2 = new List<string>() { "bbb", "bbb", "bbb" };
+            var actual = ListPraktika.Program.CombineLists(fake1, fake2);
+            var expected = new List<string> { "aaa", "aaa", "aaa", "bbb", "bbb", "bbb" };
             CollectionAssert.AreEqual(expected, actual);
         }
     }
