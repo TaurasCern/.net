e9a0af41b5e8eef73adce249565fa6dab4460d30
Taurenxoo <t.cerniauskas@yahoo.com>
2022-09-13 22:36:21 +0300

    praktika

--- OOP/LINQ/LINQ/Program.cs
+++ OOP/LINQ/LINQ/Program.cs
@@ -4,12 +4,17 @@
     {
         static void Main(string[] args)
         {
-            List<string> list = new List<string>() { "dangus", "afro", "vanduo", "darzelis", "darzove", "kremas", "valdiklis", "daumantas", "mokinys", "pazymys", "danguole" };
-
-            Console.WriteLine(string.Join(",", Filter3rd(list)));
+            //List<string> list = new List<string>() { "dangus", "afro", "vanduo", "darzelis", "darzove", "kremas", "valdiklis", "daumantas", "mokinys", "pazymys", "danguole" };
+            int[] arr = { 3, 9, 2, 8, 6, 5, 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            //Console.WriteLine(string.Join(",", ));
+            Console.WriteLine(String.Join(",", Filter7th(arr)));
         }
         public static List<int> Filter1st(List<int> list) => list.Where(n => n >= 35 && n <= 99).ToList();
         public static List<string> Filter2nd(List<string> list) => list.Where(c => c.Length > 4).Select(c => c.ToUpper()).ToList();
         public static List<string> Filter3rd(List<string> list) => list.Where(x => x.StartsWith("d") && x.EndsWith("s")).ToList();
+        public static int[] Filter4th(int[] numbers) => (from n in numbers where n % 2 == 0 select n).ToArray();
+        public static int[] Filter5th(int[] numbers) => (from n in numbers where n > 0 select n).ToArray();
+        public static int[] Filter6th(int[] numbers) => (from n in numbers select n * n).ToArray();
+        public static Dictionary<int, int> Filter7th(int[] numbers) => (from n in numbers group n by n into g select new { Number = g.Key, Count = (from n in g select n).Count()}).ToDictionary(n => n.Number, n => n.Count);
     }
 }
--- OOP/LINQ/LINQ_Tests/UnitTest1.cs
+++ OOP/LINQ/LINQ_Tests/UnitTest1.cs
@@ -33,5 +33,56 @@ namespace LINQ_Tests
 
             CollectionAssert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Exercise4()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] expected = { 0, 2, 4, 6, 8 };
+
+            var actual = LINQ.Program.Filter4th(arr);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Exercise5()
+        {
+            int[] arr =  { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            int[] expected = { 1, 3, 12, 19, 6, 9, 10, 14 };
+
+            var actual = LINQ.Program.Filter5th(arr);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Exercise6()
+        {
+            int[] arr = { 3, 9, 2, 8, 6, 5 };
+            int[] expected = { 9, 81, 4, 64, 36, 25 };
+
+            var actual = LINQ.Program.Filter6th(arr);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Exercise7()
+        {
+            int[] arr = { 3, 9, 2, 8, 6, 5, 5, 9, 1, 2, 3, 7, 5, 6, 7, 3, 7, 6, 8, 5, 4, 9, 6, 2 };
+            var expected = new Dictionary<int, int>()
+            {
+                { 3, 3 },
+                { 9, 3 },
+                { 2, 3 },
+                { 8, 2 },
+                { 6, 4 },
+                { 5, 4 },
+                { 1, 1 },
+                { 7, 3 },              
+                { 4, 1 },
+            };
+
+            var actual = LINQ.Program.Filter7th(arr);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
     }
 }
